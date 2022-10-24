0e27aabfc659e0ca473a205615145b51157923c0
Oludayo <akinpelu.dayo11@gmail.com>
2022-10-24 23:10:43 +0100

    First Commit

---
+++ src/common/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Head from "./Head"
+import Search from "./Search"
+import Navbar from "./Navbar"
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <>
+        <Head />
+        <Search />
+        <Navbar />
+    </>
+  )
+}
+
+export default Header
